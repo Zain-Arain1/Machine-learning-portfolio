b221f5c120659471e4596509d30c0b048183a19a
Kunj Desai <kunjd2803@gmail.com>
2025-11-29 15:44:55 +0530

    Update import paths for component styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 
   import { createRoot } from "react-dom/client";
   import "./index.css";
-  import "./components/Hero.css";
-  import "./components/Gallery.css";
-  import "./components/About.css";
-  import "./components/Intro.css";
+  import "../components/Hero.css";
+  import "../components/Gallery.css";
+  import "../components/About.css";
+  import "../components/Intro.css";
   import App from "./App.tsx";
   createRoot(document.getElementById("root")!).render(<App />);
   
